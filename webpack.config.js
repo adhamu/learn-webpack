683a78c4d1eb5cef0732dc89d691c8ca8d6a7692
Amit Dhamu <dev@amitd.co>
2018-02-26 20:50:21 +0000

    Really on the verge of fuckin webpack off altogether

--- webpack.config.js
+++ webpack.config.js
@@ -1,66 +1,59 @@
-const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const path = require('path');
+let webpack = require('webpack');
+let path = require('path');
+let ExtractTextPlugin = require('extract-text-webpack-plugin');
+let ManifestPlugin = require('webpack-manifest-plugin');
+let CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    entry: ['./src/js/index.js', './src/scss/styles.scss'],
+    entry: {
+        app: ['./src/js/index.js'],
+        styles: ['./src/scss/styles.scss']
+    },
     output: {
-        filename: 'js/index.js',
-        path: path.join(__dirname, './dist/')
+        path: path.resolve(__dirname, './dist'),
+        filename: '[name].[chunkhash].js'
     },
     devServer: {
         contentBase: './dist'
     },
     module: {
-        rules: [{
-            test: /\.html$/,
-            include: path.resolve(__dirname, 'src'),
-            use: [{
-                loader: 'html-loader',
-                options: {
-                    minimize: true
-                }
-            }]
-        }, {
-            test: /\.(png|jpe?g)/i,
-            include: path.resolve(__dirname, 'src'),
-            use: [{
-                loader: 'url-loader',
-                options: {
-                    name: 'img/logo.png',
-                    limit: 10000
-                }
-            }, {
-                loader: 'img-loader'
-            }]
-        }, {
-            test: /\.scss$/,
-            include: path.resolve(__dirname, 'src'),
-            use: ExtractTextPlugin.extract({
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: ['node_modules'],
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.s[ac]ss$/,
+                exclude: ['node_modules'],
+                use: ExtractTextPlugin.extract({
+                    use: ['css-loader', 'postcss-loader', 'sass-loader']
+                })
+            },
+            {
+                test: /\.html$/,
+                loader: 'html-loader'
+            },
+            {
+                test: /\.(png|jpe?g)$/i,
                 use: [
-                {
-                    loader: 'css-loader',
-                    options: { minimize: true }
-                },
-                { loader: 'postcss-loader' },
-                { loader: 'sass-loader' }
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            name: 'img/[name].[hash].png',
+                            limit: 10000
+                        }
+                    },
+                    'img-loader'
                 ]
-            })
-        }, {
-            test: /\.js$/,
-            include: path.resolve(__dirname, 'src'),
-            use: {
-              loader: 'babel-loader',
-              options: {
-                presets: ['env']
-              }
             }
-        }]
+        ]
     },
     plugins: [
         new webpack.optimize.ModuleConcatenationPlugin(),
         new ExtractTextPlugin({
-            filename: 'css/styles.css'
-        })
+            filename: '[name].[chunkhash].css'
+        }),
+        new ManifestPlugin()
     ]
 };
